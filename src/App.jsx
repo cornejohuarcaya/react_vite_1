08a0ec38f9a4401e7a956b954d3b000ee707eafe
Erickcornejo <cornejo.huarcaya.erick@gmail.com>
2025-05-05 23:15:44 -0500

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Apps() {
+ 
+  return (
+    <>
+      <div>
+        <p>Holaaaaaaaa</p>
+      </div>
+      <h1>ejemplo</h1>
+    </>
+  )
+}
+
+export default Apps
