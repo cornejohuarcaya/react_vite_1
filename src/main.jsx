3b983f3b882c22ab1c111bf1824169cbdad28bed
Erickcornejo <cornejo.huarcaya.erick@gmail.com>
2025-05-07 01:45:04 -0500

    creación de componentes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Apps from './App.jsx'
+ import { MovieList } from './components/MovieList'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Apps />
+    <MovieList />
   </StrictMode>,
 )
